ff58f22b0c877829d6ce5d5db0854a129e80aa86
MAC <mac@MACdeMacBook-Air.local>
2018-01-04 10:30:24 +0800

    implement backend CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,36 @@
+class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+ def new
+   @product = Product.new
+ end
+ def create
+   @prodcut = Product.new(product_params)
+   if @prodcut.save
+     redirect_to admin_products_path
+   else
+     render :new
+   end
+ end
+ def edit
+   @product = Product.find(params[:id])
+ end
+
+ def update
+   @product = Product.find(params[:id])
+
+   if @product.update(product_params)
+     redirect_to admin_products_path
+   else
+     render :edit
+   end
+ end
+
+
+private
+
+def product_params
+  params.require(:product).permit(:title, :description, :quantity, :price)
+end
+end
