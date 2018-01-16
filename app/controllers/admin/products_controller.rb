f8b0792088deaa1bc3affae661eefbad54db4b2e
MAC <mac@MACdeMacBook-Air.local>
2018-01-16 21:31:08 +0800

    add add_to_cart button

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -24,11 +24,16 @@ class Admin::ProductsController < ApplicationController
    @product = Product.find(params[:id])
 
    if @product.update(product_params)
-     redirect_to admin_product_path(@product)
+     redirect_to admin_products_path
    else
      render :edit
    end
  end
+ def destroy
+  @product = Product.find(params[:id])
+  @product.destroy
+  redirect_to admin_products_path
+end
 
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'products#index'
+  devise_for :users
+
   namespace :admin do
     resources :products
   end
-  resources :products
+  
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
